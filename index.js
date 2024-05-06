d806c1f014d4874fa48eb5d3fe4006c323d5eec4
Rahul Choudhary <75556971+Rahulengineer-spec@users.noreply.github.com>
2024-05-07 00:16:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const capitalize = (str, lower = false) =>
   (lower ? str.toLowerCase() : str).replace(/(?:^|\s|["'([{])+\S/g, (match) =>
     match.toUpperCase()
   );
+
 submitBtn.addEventListener("click", () => {
   const val = capitalize(userName.value);
   //check if the text is empty or not
@@ -15,6 +16,7 @@ submitBtn.addEventListener("click", () => {
     userName.reportValidity();
   }
 });
+
 const generatePDF = async (name) => {
   const existingPdfBytes = await fetch("./cert.pdf").then((res) =>
     res.arrayBuffer()
@@ -31,32 +33,38 @@ const generatePDF = async (name) => {
   // Get the first page of the document
   const pages = pdfDoc.getPages();
   const firstPage = pages[0];
-
-  const certificateNumber = "RKCER" + Math.floor(Math.random() * 1000).toString().padStart(3, "0");
-
+  const certificateNumber =
+    "RKCER" + Math.floor(Math.random() * 1000).toString().padStart(3, "0");
   // Draw the certificate number
   firstPage.drawText(certificateNumber, {
     x: 168,
-    y: 227, // Adjust position as needed  320
+    y: 227, // Adjust position as needed
     size: 12, // Adjust size as needed
     font: SanChezFont,
     color: rgb(0, 0, 0),
   });
-
   // Draw a string of text diagonally across the first page
   firstPage.drawText(name, {
-    x: 412,//290
+    x: 412, //290
     y: 267,
     size: 32,
     font: SanChezFont,
     color: rgb(0.2, 0.84, 0.67),
   });
+  
+  // Add current date
+  const currentDate = new Date().toLocaleDateString();
+  firstPage.drawText(currentDate, {
+    x: 678,
+    y: 142,
+    size: 12,
+    font: SanChezFont,
+    color: rgb(0, 0, 0),
+  });
+
   // Serialize the PDFDocument to bytes (a Uint8Array)
   const pdfBytes = await pdfDoc.save();
   console.log("Done creating");
-  // this was for creating uri and showing in iframe
-  // const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
-  // document.getElementById("pdf").src = pdfDataUri;
   var file = new File(
     [pdfBytes],
     "RK INSTITUION Certificate.pdf",
@@ -64,6 +72,5 @@ const generatePDF = async (name) => {
       type: "application/pdf;charset=utf-8",
     }
   );
- saveAs(file);
+  saveAs(file);
 };
-// init();
